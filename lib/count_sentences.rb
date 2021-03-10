94dc1fdf3f1c26ea8a0b90e2a460b7ba5c1975ce
Clayon Stewart <c.stewart89@gmail.com>
2021-03-10 19:07:07 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    cases = [".","...","!","!!","?"]
+    array = []
+    cases.each do |i|
+      if self.split(i)
+        array << self.split(i)
+      end
+    end
+   array.length
   end
 end
